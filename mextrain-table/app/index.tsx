88e70797c10902e1be5f96787601c2d171e951bb
jbrandonb <brandonbeene@outlook.com>
2026-02-12 08:53:06 -0800

    Welcome_landing

---
+++ mextrain-table/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function TabIndex() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+    >
+      <Text style={{ fontSize: 24 }}>Welcome to MexTrain Table!</Text>
+    </View>
+  );
+}
+
